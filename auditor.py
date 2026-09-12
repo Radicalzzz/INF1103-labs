f30bffaca92b8bf34def59f3bc81b3c305950b0f
Radicalzzz <Radcliffetan@gmail.com>
2026-09-12 14:45:47 +0800

    Task 1.6

--- auditor.py
+++ auditor.py
@@ -4,10 +4,11 @@ inventory = 0
 while True:
     user_input = input("\nPlease enter the number of items to add to inventory (or type 'exit' to quit): ")
     if user_input.lower() == 'exit':
+        print("Total units in inventory:", inventory)
         break
 
     # Checks for negative numbers   
-    if user_input.startswith('-') and user_input[1:].replace('.', '').isdigit(): # Checks for int/float negative numbers
+    if user_input.startswith('-') and user_input[1:].replace('.', '').isdigit(): # Checks for int/float negative numbers by replacing the "." in case of negative float numbers
         print("Invalid input. Please enter a non-negative integer number or type 'exit'.")
         continue
 
